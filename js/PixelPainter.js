153c5e9cc9d6344d7ffba9e23097ada4dbf68c4a
KanKan <cindykan@KanKan.local>
2018-01-03 19:00:35 -1000

    grid in both swatch and grid

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,11 +1,5 @@
 //creating grid in container
-// function grid(x) {
-//   for (let rows = 0; rows < x; rows++) {
-//     for (let columns = 0; columns < x; columns++);
-//     };
-//   }
-// var btn = document.getElementById('btn');
-// btn.addEventListener("click", function(event) {
+
 
 const bodyBox = document.getElementById('pixelPainter');
 
@@ -18,26 +12,24 @@ gridBox.id = 'grid';
 bodyBox.appendChild(gridBox);
 
 function createGrid (x, y, parent) {
-  // let container = document.createElement('div');
-  // container.appendChild(gridBox);
+  let container = document.createElement('div');
+  parent.appendChild(container);
 
   for (let i = 0; i < x; i++) {
     let row = document.createElement('div');
     row.className = 'row';
-    gridBox.appendChild(row);
+    parent.appendChild(row);
 
-      for (let k = 0; k < y; k++) {
+      for (let j = 0; j < y; j++) {
         let cell = document.createElement('div');
         cell.className = 'cell';
         row.appendChild(cell);
 
       }
-
   }
-
 }
 createGrid(50, 50, gridBox);
-// createGrid(20, 20, swatchBox);
+createGrid(10, 10, swatchBox);
 
 
 
