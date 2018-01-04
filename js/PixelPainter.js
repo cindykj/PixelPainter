60a9e309bc39d5b43716305c6e908ec180b904c7
KanKan <cindykan@KanKan.local>
2018-01-03 18:35:05 -1000

    full grid completed, needs swatchbox grid

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,20 +1,50 @@
 //creating grid in container
-function grid(x) {
-  for (let rows = 0; rows < x; rows++) {
-    for (let columns = 0; columns < x; columns++);
-    };
-  }
+// function grid(x) {
+//   for (let rows = 0; rows < x; rows++) {
+//     for (let columns = 0; columns < x; columns++);
+//     };
+//   }
 // var btn = document.getElementById('btn');
 // btn.addEventListener("click", function(event) {
 
-let gridElement = document.createElement('div');
-gridElement.id = 'gridDiv';
-gridElement.style.height = '10px';
-gridElement.style.width = '10px';
-gridElement.style.border = '1px solid blue';
+const bodyBox = document.getElementById('pixelPainter');
 
-let containerElem = document.getElementById('container');
-containerElem.appendChild(gridElement);
+const swatchBox = document.createElement('div');
+swatchBox.id = 'swatch';
+bodyBox.appendChild(swatchBox);
+
+const gridBox = document.createElement('div');
+gridBox.id = 'grid';
+bodyBox.appendChild(gridBox);
+
+function createGrid (x, y, parent) {
+  // let container = document.createElement('div');
+  // container.appendChild(gridBox);
+
+  for (let i = 0; i < x; i++) {
+    let row = document.createElement('div');
+    row.className = 'row';
+    gridBox.appendChild(row);
+
+      for (let k = 0; k < y; k++) {
+        let cell = document.createElement('div');
+        cell.className = 'cell';
+        row.appendChild(cell);
+
+      }
+
+  }
+
+}
+createGrid(50, 50, gridBox);
+// createGrid(20, 20, swatchBox);
+
+
+
+
+
+
+
 
 
 
