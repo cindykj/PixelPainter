4b73d733890e77ea35b0962a111284f280f9b6f5
Mika Dumont <mikakrantman@gmail.com>
2018-01-04 20:06:29 -1000

    it works!

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -11,10 +11,9 @@ const gridBox = document.createElement('div');
 gridBox.id = 'grid';
 bodyBox.appendChild(gridBox);
 
-const swatchColor = ['#FFFFFF', '#0000FF', '#8A2BE2'];
-const blanco = '#0000FF';
+const swatchColor = ['#7FFFD4', '#0000FF', '#8A2BE2'];
+const blanco = '#FFFFFF';
 let storedColor;
-
 
 //make swatch grid
 
@@ -58,58 +57,53 @@ function createGrid(x, y, parent, blanco) {
       cell.className = 'paintCell';
       cell.style.backgroundColor = blanco;
       cell.addEventListener('click', colorPicker2);
+      cell.addEventListener('mouseover', colorPicker2);
       row.appendChild(cell);
     }
   }
 };
 
-// clear 
+// Clear 
+
 let clear = document.createElement('button');
 clear.id = 'clear';
 clear.innerHTML = 'Clear';
 document.body.appendChild(clear);
 
 clear.addEventListener('click', function () {
-  console.log('clearing');
   let myGrid = document.querySelectorAll('.paintCell');
   for (let i = 0; i < myGrid.length; i++) {
     myGrid[i].style.backgroundColor = '#FFFFFF'
   }
 })
 
+// Erase
+
+let erase = document.createElement('button');
+erase.id = 'erase';
+erase.innerHTML = 'Erase';
+document.body.appendChild(erase);
+
+erase.addEventListener('click', function () {
+  storedColor = 'white'
+
+});
 
 //take clicked color and assign to variable
 function colorPicker(event) {
-  console.log('asdlfjaslkdj')
   storedColor = event.target.style.backgroundColor;
 }
 
 //assign event listener to grid
-
-
-//
 function colorPicker2(event) {
-  console.log('hjkhkjhkjhk')
   event.target.style.backgroundColor = storedColor;
 }
 
 createSwatchGrid(1, 3, swatchBox, swatchColor);
 createGrid(8, 8, gridBox, blanco);
 
-//erase
 
-// let erase = document.createElement('button');
-// erase.id = 'erase';
-// erase.innerHTML = 'Erase';
-// document.body.appendChild(erase);
 
-// erase.addEventListener('click', function () {
-//   console.log('erasing');
-//   if (event.target === paintCell) {
-//     changeColor(storedColor, event.target)
-//   }
-
-// });
 
 
 
